59ff07d9c536c6e7f03932ea42326a7130d89677
George Lioris <george.lioris@gmail.com>
2020-06-05 19:49:02 +0300

    Display appropriate comparisons between steps

--- src/lib/algorithms/sorting.js
+++ src/lib/algorithms/sorting.js
@@ -1,7 +1,7 @@
 const sorting = {
   selectionSort: inputArr => {
     const array = [...inputArr];
-    const data = { 0: { step: inputArr, pointer: 0 } };
+    const data = {};
 
     for (let i = 0; i < array.length; i += 1) {
       let minIndex = i;
@@ -10,28 +10,33 @@ const sorting = {
         if (array[j] < array[minIndex]) minIndex = j;
       }
 
-      if (minIndex !== i)
+      if (minIndex !== i) {
+        const step = [...array];
         [array[i], array[minIndex]] = [array[minIndex], array[i]];
-      data[Object.keys(data).length] = {
-        step: [...array],
-        pointer: { i, j: minIndex }
-      };
+        data[Object.keys(data).length] = {
+          step,
+          pointer: { i, j: minIndex }
+        };
+      }
     }
+
+    data[Object.keys(data).length] = { step: [...array], pointer: null };
 
     return data;
   },
 
   bubbleSort: inputArr => {
     const array = [...inputArr];
-    const data = { 0: { step: inputArr, pointer: 0 } };
+    const data = {};
     let swapped = false;
 
     for (let i = 0; i < array.length; i += 1) {
       for (let j = 0; j < array.length - i; j += 1) {
         if (array[j + 1] < array[j]) {
+          const step = [...array];
           [array[j], array[j + 1]] = [array[j + 1], array[j]];
           data[Object.keys(data).length] = {
-            step: [...array],
+            step,
             pointer: { i: j, j: j + 1 }
           };
           swapped = true;
@@ -40,6 +45,8 @@ const sorting = {
 
       if (!swapped) return data;
     }
+
+    data[Object.keys(data).length] = { step: [...array], pointer: null };
 
     return data;
   }
