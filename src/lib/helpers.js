475c9f51d49e3d8831ef0ceeb156afdf375f303b
George Lioris <george.lioris@gmail.com>
2020-06-24 22:09:40 +0300

    Variable rename

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -46,12 +46,12 @@ export function merge(left, right, data, array) {
 }
 
 export function shuffle(array) {
-  const nums = [...array];
-  for (let i = nums.length - 1; i > 0; i -= 1) {
+  const input = [...array];
+  for (let i = input.length - 1; i > 0; i -= 1) {
     const j = Math.floor(Math.random() * (i + 1));
-    [nums[i], nums[j]] = [nums[j], nums[i]];
+    [input[i], input[j]] = [input[j], input[i]];
   }
-  return nums;
+  return input;
 }
 
 export const formatValues = (initNums) => ({
